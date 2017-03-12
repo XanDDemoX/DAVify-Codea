590a9aded23546e17a9b41fe46a8adbf5e079515
XanDDemoX <XanDDemoX@users.noreply.github.com>
2017-03-12 22:51:37 +0000

    Implement Windows map as network drive
    Implements listing projects (excluding collections like Examples),
    listing project files, file sizes and faked created /modified dates
    and opening files for reading.
    Also implements propfind for files, and sending header and content
    seperately for 207 responses.
    HttpServer now uses objects for both requests and responses.

--- WebDavServer.lua
+++ WebDavServer.lua
@@ -18,11 +18,11 @@ function WebDavServer:init(...)
         end
     end,...)
     self.name = "Codea-WebDav-HTTP/1.1"
+    self.date = os.date("%Y-%m-%dT%H:%M:%S+00:00")
 end
 
 function WebDavServer:get(request) 
     local parts = url.parse_path(request.path)
-    print(#parts)
     if #parts ~= 2 then
         return nil
     end
@@ -30,18 +30,20 @@ function WebDavServer:get(request)
     local fullFileName = parts[2]
     local fileName,fileExtension = fullFileName:match("(.-)%.(.*)")
     fileExtension = fileExtension:lower()
-    
+    local content = tryReadProjectFile(project,fullFileName)
+    if content then
+        return self:response(200,"OK",content,"Content-Type",'text/xml; charset="utf8"')
+    end
+    --[[
     if fileExtension == "lua" then
         local content = tryReadProjectTab(string.format("%s:%s",project,fileName))
         if content then
             return self:response(200,"OK", content, "Content-Type",'text/lua; charset="utf8"')
         end
-    elseif fileName:lower() == "info" and fileExtension == "plist" then
-        local content = tryReadLocalFile(project,fullFileName)
-        if content then
-            return self:response(200,"OK",content,"Content-Type",'text/xml; charset="utf8"')
-        end
-    end
+          ]]--
+    --elseif fileName:lower() == "info" and fileExtension == "plist" then
+
+   -- end
     return self:error(404,"Not found")
 end
 
@@ -86,23 +88,47 @@ function WebDavServer:propFind(request)
     local parts = url.parse_path(request.path)
     local count = #parts
     local nodes = {}
+    local depth = tonumber(request.header["Depth"]) or 1
+    
     -- request for root
     if count == 0 then
         table.insert(nodes,{path="/", folder=true})
-        for i,name in ipairs(listProjects()) do
-            name = parseProjectName(name)
-            table.insert(nodes,{path=string.format("/%s/",name), folder=true})
-        end
-    elseif count > 0 then
-        local project = parts[1]
-        if count == 1 then
-            table.insert(nodes,{path=string.format("/%s/",project), folder=true})
-            table.insert(nodes, {path=string.format("/%s/Info.plist",project)})
-            for i, name in ipairs(listProjectTabs(project)) do
-                table.insert(nodes,{path=string.format("/%s/%s.lua",project,name)})
+        if depth == 1 then
+            local folders = listProjects()
+            for i,name in ipairs(folders) do
+                if not name:find(":") then -- filter out collections for now e.g Examples 
+                --name = parseProjectName(name)
+                    table.insert(nodes,{path=string.format("/%s/",name), folder=true})
+                end
             end
-        else
+        elseif depth > 1 then
             return nil
+        end
+    elseif count > 0 then -- no support for paths > 2 
+        local isFile = parts[count]:find("%.") ~= nil
+        if isFile then
+            local project = parts[1]
+            local fileName = parts[2]
+            local size = tryGetProjectFileSize(project,fileName)
+            if not size then
+                return self:error(404,"Not found")
+            end
+            table.insert(nodes,{path=string.format("/%s/%s.lua",project,fileName),size=size})
+        else
+            local project = parts[1]
+            if count == 1 then
+                table.insert(nodes,{path=string.format("/%s/",project), folder=true})
+                local plistSize = tryGetProjectFileSize(project,"Info.plist")
+                if plistSize then
+                    table.insert(nodes, {path=string.format("/%s/Info.plist",project), size=plistSize})
+                end
+                for i, fileName in ipairs(listProjectTabs(project)) do
+                    local size = tryGetProjectFileSize(project,string.format("%s.lua",fileName))
+                    table.insert(nodes,{path=string.format("/%s/%s.lua",project,fileName),size=size})
+                end
+            else
+                return nil
+            end
         end
     end
     local builder = XmlBuilder()
@@ -110,22 +136,28 @@ function WebDavServer:propFind(request)
     :elem("multistatus")
     :attr("xmlns:D","DAV:")
     :push()
-    
+    local date = self.date
     for i,node in ipairs(nodes) do
         builder:elem("response"):push()
             builder:elem("href",node.path)
             builder:elem("propstat"):push()
                 builder:elem("prop"):push()
+                    builder:elem("creationdate",date)
                     if node.folder then
                         builder:elem("resourcetype"):push():elem("collection"):pop()
+                    else
+                        builder:elem("getlastmodified",date)
+                        if node.size then
+                            builder:elem("getcontentlength",node.size)
+                        end
                     end
-                    builder:elem("creationdate","2017-02-08T00:23:12+00:00")
                 builder:pop()
                 builder:elem("status","HTTP/1.1 200 OK")
             builder:pop()
         :pop()
     end
-    return self:response(207,"Multi Status",builder:toString(),"Content-Type",'text/xml; charset="utf8"')
+    return self:response(207,"Multi Status",builder:toString(),
+    "Content-Type",'application/xml; charset="utf-8"')
 end 
 
 
