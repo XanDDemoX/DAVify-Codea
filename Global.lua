e892de6636b912261cabb853180b0dc0c1b01758
XanDDemoX <XanDDemoX@users.noreply.github.com>
2017-03-12 22:51:37 +0000

    Implement Windows map as network drive
    Implements listing projects (excluding collections like Examples),
    listing project files, file sizes and faked created /modified dates
    and opening files for reading.
    Also implements propfind for files, and sending header and content
    seperately for 207 responses.
    HttpServer now uses objects for both requests and responses.

--- Global.lua
+++ Global.lua
@@ -7,11 +7,24 @@ function tryReadProjectTab(key)
     return result and content or nil
 end
 
-function tryReadLocalFile(project,fileName)
-    local path = string.format("%s/Documents/%s.codea/%s",os.getenv("HOME"),project,fileName)
+local function formatFilePath(project,fileName)
+    return string.format("%s/Documents/%s.codea/%s",os.getenv("HOME"),project,fileName)
+end
+
+function tryReadProjectFile(project,fileName)
+    local path = formatFilePath(project,fileName)
     local stream = io.open(path,"r")
     if not stream then return nil end
     local content = stream:read("*all")
     stream:close()
     return content
+end
+
+function tryGetProjectFileSize(project, fileName)
+    local path = formatFilePath(project,fileName)
+    local stream = io.open(path,"r")
+    if not stream then return nil end
+    local size = stream:seek("end")
+    stream:close()
+    return size
 end
