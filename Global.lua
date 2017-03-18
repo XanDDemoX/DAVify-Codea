7a3d4bd176f7aab9e596900fde05458953bbf99e
XanDDemoX <XanDDemoX@users.noreply.github.com>
2017-03-18 14:21:39 +0000

    Implement virtual file system

--- Global.lua
+++ Global.lua
@@ -7,24 +7,13 @@ function tryReadProjectTab(key)
     return result and content or nil
 end
 
-local function formatFilePath(project,fileName)
-    return string.format("%s/Documents/%s.codea/%s",os.getenv("HOME"),project,fileName)
-end
-
-function tryReadProjectFile(project,fileName)
-    local path = formatFilePath(project,fileName)
-    local stream = io.open(path,"r")
-    if not stream then return nil end
-    local content = stream:read("*all")
-    stream:close()
-    return content
-end
-
-function tryGetProjectFileSize(project, fileName)
-    local path = formatFilePath(project,fileName)
-    local stream = io.open(path,"r")
-    if not stream then return nil end
-    local size = stream:seek("end")
-    stream:close()
-    return size
+function packLookup(...)
+    local count = select("#",...)
+    assert(count & 1 == 0, "Number of arguments must be even")
+    local lookup = {}
+    for i=1, count, 2 do
+        local key,value = select(i,...)
+        lookup[key] = value
+    end
+    return lookup
 end
