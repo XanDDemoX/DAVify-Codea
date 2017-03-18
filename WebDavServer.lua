2553eae2a6303df8e4e9a2d3173deecdf3f05cef
XanDDemoX <XanDDemoX@users.noreply.github.com>
2017-03-18 22:51:36 +0000

    Implement MKCOL

--- WebDavServer.lua
+++ WebDavServer.lua
@@ -5,20 +5,29 @@
 -- http://www.webdav.org/specs/rfc3648.html
 -- http://stackoverflow.com/questions/10144148/example-of-a-minimal-request-response-cycle-for-webdav
 
+
 WebDavServer = class(HttpServer)
 
 function WebDavServer:init(folder,...)
     assert(type(folder)=="table" and folder.is_a and folder:is_a(FolderNode),
     "'folder' must not be null and derive from FolderNode.")
+    
     HttpServer.init(self, function(request)
-        if request.method == "GET" then
+        local method = request.method
+        if method == "GET" then
             return self:get(request)
-        elseif request.method == "OPTIONS" then
+        elseif method == "MKCOL" then
+            return self:mkcol(request)
+        elseif method == "OPTIONS" then
             return self:options(request)
-        elseif request.method == "PROPFIND" then
+        elseif method == "PROPFIND" then
             return self:propFind(request)
         else
-            return HttpResponse(405,"","Allow","OPTIONS, GET, PROPFIND")
+            local node = self.folder:get(request.path)
+            if not node then
+                return HttpResponse(404)
+            end
+            return HttpResponse(501,"","Allow",self:getAllow(node)) -- not implemented
         end
     end,...)
     self.name = "Codea-WebDAV-HTTP/1.1"
@@ -27,7 +36,6 @@ function WebDavServer:init(folder,...)
 end
 
 function WebDavServer:get(request) 
-    
     local node = self.folder:get(request.path)
     if not node then
         return HttpResponse(404)
@@ -40,19 +48,67 @@ function WebDavServer:get(request)
     return HttpGetResponse(node)
 end
 
-function WebDavServer:options(request)
-    return HttpResponse(200,"","Allow","OPTIONS, GET, PROPFIND")
+-- https://msdn.microsoft.com/en-us/library/aa142923(v=exchg.65).aspx
+function WebDavServer:mkcol(request)
+    local path = request.path
+    local node = self.folder:get(path)
+    if node then
+        return HttpResponse(405,"",self:getAllow(node)) -- folder already exists
+    end
+    local parts = url.parse_path(path)
+    local folderName = parts[#parts]
+    
+    table.remove(parts)
+    table.insert(parts,1,"")
+    local parentPath = table.concat(parts,"/")
+    local parent = self.folder:get(parentPath)
+    if not parent then
+        return HttpResponse(409,"Parent must be created before child.") -- conflict parent must be created first
+    end
+    if not parent:canCreateFolder() then
+        return HttpResponse(403)
+    end
+    local validName,created = parent:createFolder(folderName)
+    if not validName then
+        return HttpResponse(422)
+    elseif not created then
+        return HttpResponse(409,"Collection already exists.")
+    end
+    return HttpResponse(201)
 end
 
-function WebDavServer:propFind(request)
-    local depth = tonumber(request.header["Depth"])
-    if depth ~= 0 and depth ~= 1 then
-        return HttpResponse(501) -- no support for depth infinity
+function WebDavServer:getAllow(node)
+    local methods = {"OPTIONS", "PROPFIND"}
+    if node:is_a(FileNode) then
+        table.insert(methods,"GET")
+    elseif node:is_a(FolderNode) then
+        if node:canCreateFolder() then
+            table.insert(methods,"MKCOL")
+        end
     end
+    return table.concat(methods,", ")
+end
+
+function WebDavServer:options(request)
     local node = self.folder:get(request.path)
     if not node then
         return HttpResponse(404)
     end
+    return HttpResponse(200,"","Allow",self:getAllow(node))
+end
+
+function WebDavServer:propFind(request)
+    local node = self.folder:get(request.path)
+    if not node then
+        return HttpResponse(404)
+    end
+    
+    local depth = tonumber(request.header["Depth"])
+    if depth ~= 0 and depth ~= 1 then
+        -- no support for depth infinity
+        return HttpResponse(405,"","Allow",self:getAllow(node),"Allow-Depth","0,1") 
+    end
+
     local nodes = {node}
     if depth == 1 then
         if node:is_a(FolderNode) then
@@ -60,7 +116,7 @@ function WebDavServer:propFind(request)
                 table.insert(nodes,child)
             end
         else
-            return HttpResonse(501) -- no support for depth 1 for files
+            return HttpResponse(405,"","Allow",self:getAllow(node),"Allow-Depth","0")
         end
     end
     local xml = XmlBuilder()
