9831c74c19fdf8800268ccf008f2ba4de3ef0d27
XanDDemoX <XanDDemoX@users.noreply.github.com>
2017-03-19 21:00:21 +0000

    Tidy up and extract a parse function for project keys

--- Global.lua
+++ Global.lua
@@ -11,3 +11,12 @@ function packLookup(...)
     end
     return lookup
 end
+
+function parseProjectKey(key)
+    assert(type(key) == "string")
+    local idx = key:find(":",1,true)
+    if idx then
+        return key:sub(idx+1), key:sub(1,idx-1)
+    end
+    return key
+end
