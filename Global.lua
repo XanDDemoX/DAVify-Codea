9eb8e142e642a64ea5934e499bdd21cb6be8bc3d
XanDDemoX <XanDDemoX@users.noreply.github.com>
2017-03-19 14:26:16 +0000

    Remove unreferenced function

--- Global.lua
+++ Global.lua
@@ -1,12 +1,6 @@
 socket = require("socket")
 url = socket.url
 
-function tryReadProjectTab(key)
-    print(key)
-    local result, content = xpcall(readProjectTab,function()end, key)
-    return result and content or nil
-end
-
 function packLookup(...)
     local count = select("#",...)
     assert(count & 1 == 0, "Number of arguments must be even")
