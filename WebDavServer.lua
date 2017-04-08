cd3d5c3b9481eb41ec788e57839d86e68ae2b10c
XanDDemoX <XanDDemoX@users.noreply.github.com>
2017-04-08 14:03:29 +0100

    Code format

--- WebDavServer.lua
+++ WebDavServer.lua
@@ -6,9 +6,7 @@
 -- http://stackoverflow.com/questions/10144148/example-of-a-minimal-request-response-cycle-for-webdav
 -- http://sabre.io/dav/clients/windows/
 
-
 WebDavServer = class(HttpServer)
-
 function WebDavServer:init(folder,...)
     assert(type(folder)=="table" and folder.is_a and folder:is_a(FolderNode),
     "'folder' must not be null and derive from FolderNode.")
